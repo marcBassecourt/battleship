2bcf530271e551af364bebc60b961aad7b12513c
marcBassecourt <marc.bassecourt@etu.upmc.fr>
2019-12-23 16:29:03 +0100

    Premier Jet

---
+++ Bateau.hh
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+
+
+class Bateau {
+protected:
+  size_t _id;
+  static int _cpt_id;
+  int taille;
+public:
+  Bateau():_id(_cpt_id++);
+  Bateau(int t):_id(_cpt_id++),taille(t);
+
+
+  virtual void toString() const = 0;
+};
